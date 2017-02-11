6c57902aa5f13d2626f3377c3b868590a43356bd
Wei Wang <weiwa@cse.ust.hk>
2017-02-10 23:23:13 -0800

    revise test.sh

--- test.sh
+++ test.sh
@@ -6,43 +6,52 @@
 # Run the user program to copy the file
 # Compare the md5sum of the file and its copy
 
-# Generate a random file
-userDomain="hk.ust.weiwa"
+userDomain="hk.ust.comp4651"
 dummyFile="dummy"
+
+# Clean up
+clean_up() {
+	rm -f $dummyFile
+	rm -f md5.txt
+	hadoop fs -rm -f $dummyFile
+}
+
+clean_up
+# Generate a random file
 head -c 10M < /dev/urandom > $dummyFile
 md5sum $dummyFile > md5.txt
 
-maven=`mvn clean package`
+# Compile and package to jar
+mvn clean package
 if [ "$?" -ne 0 ]; then
 	echo "[ERROR] mvn clean package UNSUCCESSFUL!" >> error
-	echo "" >> error
-	echo $maven >> error
+	clean_up
 	exit 1
 fi
 
 # Copy the local file to HDFS
-hdfs=`hadoop fs -moveFromLocal $dummyFile ./`
+hadoop fs -moveFromLocal $dummyFile ./
 if [ "$?" -ne 0 ]; then
-	echo "[ERROR] Cannot move a file to HDFS!" >> error
-	echo "" >> error
-	echo $hdfs >> error
+	echo "[ERROR] Cannot move a test data to HDFS!" >> error
+	clean_up
 	exit 1
 fi
 
 # Run the user program to copy the file from HDFS back to local disk
-hadoop=`hadoop jar target/FileSystemAPI-1.0-SNAPSHOT.jar $userDomain.CopyFile $dummyFile $dummyFile`
+hadoop jar target/FileSystemAPI-1.0-SNAPSHOT.jar $userDomain.CopyFile $dummyFile $dummyFile
 if [ ! -f $dummyFile ]; then
-	echo "[ERROR] Copy from HDFS failed!" >> error
-	echo "" >> error
-	echo $hadoop >> error
+	echo "[ERROR] No file has been copied from HDFS!" >> error
+	clean_up
 	exit 1
 fi
 
 md5sum -c md5.txt
 if [ "$?" -ne 0 ]; then
 	echo "[ERROR] Faild to pass the md5sum check!" >> error
+	clean_up
 	exit 1
 fi
 
-rm -f $dummyFile
+clean_up
 echo "[SUCCESS]"
+exit 0
